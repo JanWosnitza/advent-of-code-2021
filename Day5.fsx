4da9f0435cc098a4ac0c90c0ea02f2a70567713a
Jan Wosnitza <jan_wosnitza@hotmail.com>
2021-12-08 20:34:35 +0100

    add day 5

---
+++ Day5.fsx
@@ -0,0 +1,59 @@
+// https://adventofcode.com/2021/day/5
+#load "Util.fsx"
+#r "nuget: FSharp.Text.RegexProvider"
+
+let day = Util.adventDay 5 """
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
+
+type Regex = FSharp.Text.RegexProvider.Regex< @"^(?<X1>\d+),(?<Y1>\d+) -> (?<X2>\d+),(?<Y2>\d+)$" >
+
+type Vent = {X1 : int; Y1 : int; X2 : int; Y2 : int}
+
+let vents =
+    day.RawInput
+    |> Array.map (fun x ->
+        let tm = Regex().TypedMatch(x)
+        {
+            X1 = tm.X1.Value |> int
+            Y1 = tm.Y1.Value |> int
+            X2 = tm.X2.Value |> int
+            Y2 = tm.Y2.Value |> int
+        }
+    )
+
+let getPositions (vent:Vent) =
+    let signX = sign (vent.X2 - vent.X1)
+    let signY = sign (vent.Y2 - vent.Y1)
+    let size = if vent.X2 <> vent.X1 then abs (vent.X2 - vent.X1) else abs (vent.Y2 - vent.Y1)
+    seq { for i = 0 to size do yield (vent.X1 + i * signX, vent.Y1 + i * signY) }
+
+module Part1 =
+    let vents =
+        vents
+        |> Seq.filter (fun vent -> vent.X1 = vent.X2 || vent.Y1 = vent.Y2)
+
+module Part2 =
+    let vents = vents
+        
+
+let overlaps (vents) =
+    vents
+    |> Seq.collect getPositions
+    |> Seq.countBy id
+    |> Seq.filter (fun (_, count) -> count >= 2)
+    |> Seq.length
+
+day.Answer(
+    part1 = overlaps Part1.vents,
+    part2 = overlaps Part2.vents
+)
